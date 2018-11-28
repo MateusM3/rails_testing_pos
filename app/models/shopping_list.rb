f21ffa8a3f597251fc88b32f04a21d0d2847141c
Mateus Monteiro <mateusmonteirom3@gmail.com>
2018-11-28 00:00:11 -0200

    Adding in product reference to shopping_list model.

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,2 +1,4 @@
 class ShoppingList < ActiveRecord::Base
+  has_many :products, dependent: :destroy
+  accepts_nested_attributes_for :products, reject_if: lambda { |a| a[:name].blank? }, allow_destroy: true
 end
