8ae1149cbdce60897f107d8a99cac44efeb0b43a
Mateus Monteiro <mateusmonteirom3@gmail.com>
2018-11-27 23:55:07 -0200

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181125235227) do
+ActiveRecord::Schema.define(version: 20181127010128) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "quantity"
+    t.integer  "shopping_list_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "products", ["shopping_list_id"], name: "index_products_on_shopping_list_id"
 
   create_table "shopping_lists", force: :cascade do |t|
     t.string   "title"
